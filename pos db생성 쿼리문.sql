9891284d4eef0f66f90fc4c9881fb4a9eb0987c8
kimDani <rlaeksl0124@naver.com>
2024-12-10 10:37:33 +0900

    쿼리문업데이트

--- pos db생성 쿼리문.sql
+++ pos db생성 쿼리문.sql
@@ -1,101 +1,130 @@
-CREATE TABLE `Category` (
-  `category_id` int NOT NULL AUTO_INCREMENT,
-  `category_id2` int DEFAULT NULL,
-  `category_name` varchar(20) NOT NULL,
-  `is_visible` char(1) NOT NULL DEFAULT 'Y',
-  PRIMARY KEY (`category_id`),
-  KEY `fk_category_self` (`category_id2`),
-  CONSTRAINT `fk_category_self` FOREIGN KEY (`category_id2`) REFERENCES `Category` (`category_id`)  ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table category
+(
+    category_id  int auto_increment primary key,
+    isvisible    tinyint(1) default 0 null,
+    parent       int                  null,
+    categoryname varchar(255)         null,
+    constraint FKcmtwvovxcdalvlgifxh6luun8
+        foreign key (parent) references category (category_id)
+            on delete cascade
+);
 
-CREATE TABLE `Dining` (
-  `Table_no` int NOT NULL,
-  `x_position` int NOT NULL,
-  `y_position` int NOT NULL,
-  `table_color` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`Table_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table dining
+(
+    tableNo        int       primary key  					  not null,
+    xPosition      decimal(10, 2)                             not null,
+    yPosition      decimal(10, 2)                             not null,
+    tableColor     varchar(20)                                null,
+    width          decimal(10, 2)                             not null,
+    height         decimal(10, 2)                             not null,
+    status         enum ('EMPTY', 'OCCUPIED') default 'EMPTY' null,
+    currentOrderNo varchar(255)                               null
+);
 
-CREATE TABLE `Menu` (
-  `menu_id` int NOT NULL AUTO_INCREMENT,
-  `category_id` int NOT NULL,
-  `menu_name` varchar(30) NOT NULL,
-  `menu_price` int NOT NULL,
-  `menu_description` varchar(300) NOT NULL,
-  `menu_image` varchar(500) NOT NULL,
-  `is_visible` char(1) NOT NULL DEFAULT 'Y',
-  `menu_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`menu_id`),
-  KEY `fk_category_id` (`category_id`),
-  CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `Category` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table menu
+(
+    menuId          int auto_increment	 primary key,
+    category_id     int                  not null,
+    menuName        varchar(30)          not null,
+    menuPrice       int                  not null,
+    menuDescription varchar(300)         null,
+    menuImage       varchar(500)         null,
+    isVisible       tinyint(1) default 0 null,
+    menuDate        date                 null,
+    constraint FKww84tou7nixng06lmxawvcre
+        foreign key (category_id) references category (category_id)
+);
 
-CREATE TABLE `Menu_status` (
-  `menu_status_num` int NOT NULL,
-  `menu_id` int NOT NULL,
-  `menu_status` varchar(10) DEFAULT NULL,
-  `menu_start_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `menu_end_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`menu_status_num`,`menu_id`),
-  KEY `fk_menu_id` (`menu_id`),
-  CONSTRAINT `fk_menu_id` FOREIGN KEY (`menu_id`) REFERENCES `Menu` (`menu_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table menu_status
+(
+    menuStatusNum int                                not null,
+    menuId        int                                not null,
+    menuStatus    varchar(10)                        null,
+    menuStartDate datetime default CURRENT_TIMESTAMP null,
+    menuEndDate   datetime                           null,
+    primary key (menuStatusNum, menuId),
+    constraint fk_menu_id
+        foreign key (menuId) references menu (menuId)
+);
 
+create table `order`
+(
+    orderNo        varchar(255)                       not null primary key,
+    tableNo        int                                null,
+    storedTableNo  int                                null,
+    orderTime      datetime default CURRENT_TIMESTAMP not null,
+    orderPayStatus varchar(255)                       null,
+    orderAmount    double                             null,
+    orderVat       double                             null,
+    constraint fk_tableNo
+        foreign key (tableNo) references dining (tableNo)
+            on delete set null
+);
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
-     orderNo        varchar(255)                       NOT NULL,
-     tableNo        int                                NOT NULL,
-     orderTime      datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
-     orderPayStatus varchar(255)                       NULL,
-     orderAmount    double                             NULL,
-     orderVat       double
-         NULL,
-     table_no       int                                NOT NULL,
-     PRIMARY KEY (orderNo),
-     KEY fk_table_no (tableNo),
-     CONSTRAINT fk_tableNo FOREIGN KEY (tableNo) REFERENCES dining (tableNo)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+alter table dining
+    add constraint dining___fk
+        foreign key (currentOrderNo) references `order` (orderNo);
 
-DROP TABLE IF EXISTS order_detail;
-CREATE TABLE `order_detail` (
-  `ordDetailNo` int NOT NULL AUTO_INCREMENT,
-  `orderNo` varchar(255) NOT NULL,
-  `menuId` int NOT NULL,
-  `ordAddNo` varchar(4) NOT NULL,
-  `unitPrice` int NOT NULL,
-  `quantity` int NOT NULL,
-  `totalAmount` int NOT NULL,
-  `itemOrderTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ordDetailNo`),
-  UNIQUE KEY `ord_detail_no` (`ordDetailNo`,`orderNo`),
-  KEY `fk_order_no` (`orderNo`),
-  KEY `fk_order_detail_menuId` (`menuId`),
-  CONSTRAINT `fk_order_detail_menu_id` FOREIGN KEY (`menuId`) REFERENCES `menu` (`menuId`),
-  CONSTRAINT `fk_orderNo` FOREIGN KEY (`orderNo`) REFERENCES `order` (`orderNo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table order_detail
+(
+    ordDetailNo   int auto_increment
+        primary key,
+    orderNo       varchar(255)                       not null,
+    menuId        int                                not null,
+    ordAddNo      int                                not null,
+    unitPrice     int                                not null,
+    quantity      int      default 1                 not null,
+    totalAmount   int AS (unitPrice * quantity) stored,
+    itemOrderTime datetime default CURRENT_TIMESTAMP not null,
+    constraint ord_detail_no
+        unique (ordDetailNo, orderNo),
+    constraint fk_orderNo
+        foreign key (orderNo) references `order` (orderNo),
+    constraint fk_order_detail_menu_id
+        foreign key (menuId) references menu (menuId)
+);
 
-CREATE TABLE `Pay` (
-  `pay_no` binary(16) NOT NULL DEFAULT (uuid_to_bin(uuid())),
-  `pay_seqnum` int NOT NULL,
-  `order_no` varchar(13) NOT NULL,
-  `od_pay_amt` int NOT NULL,
-  `pay_stat_cd` varchar(20) NOT NULL,
-  `pay_dt` datetime NOT NULL,
-  `pay_meth_cd` varchar(10) DEFAULT NULL,
-  `pay_aprv_cd` varchar(20) DEFAULT NULL,
-  `pay_aprv_num` varchar(10) DEFAULT NULL,
-  `pay_aprv_dt` datetime DEFAULT NULL,
-  `pay_resp_cd` varchar(10) DEFAULT NULL,
-  `pay_canc_amt` int DEFAULT NULL,
-  `pay_canc_dt` datetime DEFAULT NULL,
-  `trans_type` varchar(20) NOT NULL,
-  `card_co` varchar(20) DEFAULT NULL,
-  `mth_instlmt` int DEFAULT NULL,
-  `card_num` varchar(20) DEFAULT NULL,
-  `bank_name` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`pay_no`,`pay_seqnum`),
-  KEY `fk_order_no_pay` (`order_no`),
-  CONSTRAINT `fk_order_no_pay` FOREIGN KEY (`order_no`) REFERENCES `Order` (`order_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create index fk_order_detail_menuId
+    on order_detail (menuId);
+
+create index fk_order_no
+    on order_detail (orderNo);
+
+create table order_sequence
+(
+    openDate        timestamp     not null
+        primary key,
+    isOpen          tinyint(1)    not null,
+    currentSequence int default 0 not null,
+    totalOrders     int default 0 null,
+    totalSales      int default 0 null
+);
+
+create table pay
+(
+    payNo               binary(16)                      default (uuid_to_bin(uuid())) not null,
+    paySeqnum           int                                                           not null,
+    orderNo             varchar(255)                                                  not null,
+    odPayAmt            int                                                           not null,
+    payStatCd           varchar(20)                                                   not null,
+    payDt               datetime                                                      not null,
+    payMethCd           varchar(10)                                                   null,
+    payAprvCd           varchar(20)                                                   null,
+    payAprvNum          varchar(10)                                                   null,
+    payAprvDt           datetime                                                      null,
+    payRespCd           varchar(10)                                                   null,
+    payCancAmt          int                                                           null,
+    payCancDt           datetime                                                      null,
+    transType           varchar(20)                                                   null,
+    cardCo              varchar(20)                                                   null,
+    mthInstlmt          int                                                           null,
+    cardNum             varchar(20)                                                   null,
+    bankName            varchar(20)                                                   null,
+    cash_receipt_number varchar(20)                                                   null comment '현금영수증 번호',
+    cash_receipt_type   enum ('PHONE', 'BUSINESS')                                    null comment '현금영수증 유형',
+    cash_receipt_status enum ('APPLIED', 'NOT_APPLIED') default 'NOT_APPLIED'         null comment '현금영수증 상태',
+    primary key (payNo, paySeqnum),
+    constraint fk_order_no_pay
+        foreign key (orderNo) references `order` (orderNo)
+);
 

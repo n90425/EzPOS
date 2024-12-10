54b0439fcd34fa04753898f02c9fa115b414164b
n90425 <n90425@naver.com>
2024-12-10 21:59:13 +0900

    menulist 수정

--- frontend/src/components/Product/ItemList.js
+++ frontend/src/components/Product/ItemList.js
@@ -23,12 +23,21 @@ function ItemList({ items = [], categories = [], onDelete, onEdit, onToggleVisib
                 ))}
             </div>
 
+            {/* 현재 선택한 카테고리 표시 */}
+            <div className="selected-category">
+                {selectedCategory
+                    ? `선택된 카테고리: ${categories.find((cat) => cat.categoryId === selectedCategory)?.categoryname || "알 수 없음"}`
+                    : "전체 카테고리"}
+            </div>
+
+
             {/* 메뉴 리스트 */}
             <table className="item-table">
                 <thead>
                     <tr>
                         <th>선택</th>
                         <th>이미지</th>
+                        <th>카테고리</th>
                         <th>상품명</th>
                         <th>가격</th>
                         <th>품절표시</th>
@@ -42,6 +51,7 @@ function ItemList({ items = [], categories = [], onDelete, onEdit, onToggleVisib
                         <tr key={item.menuId}>
                             <td><input type="checkbox" /></td>
                             <td>{item.image ? <img src={item.image} alt={item.menuName} /> : "이미지 없음"}</td>
+                            <td>{categories.find((cat) => cat.categoryId === item.categoryId)?.categoryname || "없음"}</td> 
                             <td>{item.menuName || "이름 없음"}</td> {/* 메뉴 이름 */}
                             <td>{item.menuPrice ? `${item.menuPrice}원` : "가격 없음"}</td> {/* 메뉴 가격 */}
                             <td>{item.stock || "없음"}</td> {/* 재고 수량 */}

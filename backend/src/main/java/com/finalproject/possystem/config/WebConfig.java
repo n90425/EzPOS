08c0a84c07427fdd602ec0e300ca4068b394caa2
n90425 <n90425@naver.com>
2025-01-10 21:06:04 +0900

    결제spl문 수정

--- backend/src/main/java/com/finalproject/possystem/config/WebConfig.java
+++ backend/src/main/java/com/finalproject/possystem/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins("http://54.180.119.159")
+                .allowedOrigins("http://54.180.119.159", "http://localhost:8081")
                 .allowedMethods("GET", "POST","PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)

f77b1cdfb44ad206449730f4850fae19eb0d8272
dani Kim <rlaeksl0124@naver.com>
2025-01-10 15:48:23 +0900

    Update WebConfig.java

--- backend/src/main/java/com/finalproject/possystem/config/WebConfig.java
+++ backend/src/main/java/com/finalproject/possystem/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost", "http://localhost:8080")
+                .allowedOrigins("http://3.34.46.145", "http://localhost:8080")
                 .allowedMethods("GET", "POST","PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
